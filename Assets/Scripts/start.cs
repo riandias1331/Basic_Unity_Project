800c5b81b15e8914fc6016274504d26a29f1a5db
Rian Dias <pereiradiasrian.@gmail.com>
2025-06-26 20:25:30 -0300

    initial commit

--- Assets/Scripts/start.cs
+++ Assets/Scripts/start.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class start : MonoBehaviour
 {
+    void main()
+    {
+        // This is the main entry point for the script
+        // You can initialize your game or perform setup tasks here
+        Debug.Log("Main method called!");
+        Debug.Log("Game started!");
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
